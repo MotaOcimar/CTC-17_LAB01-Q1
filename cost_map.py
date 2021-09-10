916d8a73774222d5108690ccd873b82358f38ee8
Ocimar <ocimarmota13@gmail.com>
2021-09-10 02:10:14 -0300

    Implementes a* core

--- cost_map.py
+++ cost_map.py
@@ -1,11 +1,58 @@
 import csv
+from math import inf, sqrt
 
 class CostMap(object):
     def __init__(self, data_filename):
-        self.id_list = []
-        self.lat_list = []
-        self.lng_list = []
+        self.nodes = {}
 
-        
-    # TODO
+        with open(data_filename, 'r', newline='') as data_file:
+            data_reader = csv.reader(data_file)
 
+            next(data_reader) # skip first line
+            for row in data_reader:
+                node_id = int(row[0])
+                node_lat = float(row[2])
+                node_lng = float(row[3])
+
+                new_node = Node(node_id, node_lat, node_lng)
+                self.nodes[node_id] = new_node
+
+    def get_node(self, node_id):
+        if node_id in self.nodes.keys():
+            return self.nodes[node_id]
+        return None
+
+    def get_successors(self, node):
+        successors = []
+        if node.id %2:
+            if node.id - 1 in self.nodes.keys():
+                successors.append(self.nodes[node.id-1])
+            if node.id + 2 in self.nodes.keys():
+                successors.append(self.nodes[node.id+2])
+        else:
+            if node.id - 2 in self.nodes.keys():
+                successors.append(self.nodes[node.id-2])
+            if node.id + 1 in self.nodes.keys():
+                successors.append(self.nodes[node.id+1])
+
+        return successors
+
+
+class Node(object):
+    def __init__(self, id, lat, lng):
+        self.id = id
+        self.lat = lat
+        self.lng = lng
+        self.path_parent = None
+        self.cost_passing_through = inf # f
+        self.cost_to_here = inf # g
+        self.cost_from_here = inf # h
+
+    def distance_to(self, node):
+        return sqrt((self.lat - node.lat) ** 2 + (self.lng - node.lng) ** 2)
+
+    def __lt__(self, other):
+        return self.cost_passing_through < other.cost_passing_through
+
+    def __eq__(self, other):
+        return self.id == other.id
--- path_planner.py
+++ path_planner.py
@@ -1,10 +1,49 @@
+import heapq
+from math import inf
+
 class PathPlanner(object):
     def __init__(self, cost_map):
-        super().__init__()
+        self.cost_map = cost_map
+
+
+    def construct_path(self):
         # TODO
-        pass
+        path = []
+
 
     def a_star(self, start_node_id, goal_node_id):
-        # TODO
-        return None, None
+        start_node = self.cost_map.get_node(start_node_id)
+        goal_node = self.cost_map.get_node(goal_node_id)
+
+        # Initializes start node
+        start_node.cost_to_here = 0 # g
+        start_node.cost_from_here = start_node.distance_to(goal_node) # h
+        start_node.cost_passing_through = start_node.cost_to_here + start_node.cost_from_here # f
+
+        # Initializes the priority queue
+        sorted_nodes = []
+        heapq.heappush(sorted_nodes, start_node)
+
+        # Iterates through the nodes with the lowest cost estimate looking for the goal
+        while len(sorted_nodes) != 0:
+            current_node = heapq.heappop(sorted_nodes)
+            if current_node.id == goal_node_id:
+                break
+            
+            # Improve its successors' estimates
+            successors = self.cost_map.get_successors(current_node)
+            for successor in successors:
+                cost_current_to_successor = current_node.distance_to(successor)
+
+                if successor.cost_to_here > current_node.cost_to_here + cost_current_to_successor:
+                    successor.cost_to_here = current_node.cost_to_here + cost_current_to_successor # g
+                    if successor.cost_from_here is inf: successor.cost_from_here = successor.distance_to(goal_node) # h
+                    successor.cost_passing_through = successor.cost_to_here + successor.cost_from_here # f
+
+                    if successor in sorted_nodes:
+                        heapq.heapify(sorted_nodes) # Updates heap
+                    else:
+                        heapq.heappush(sorted_nodes, successor)
+
+        return self.construct_path(), goal_node.cost_to_here
     
