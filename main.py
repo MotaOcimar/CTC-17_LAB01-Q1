480b0e9b5386f0eaa401049e91b01b54735e4a37
Ocimar <ocimarmota13@gmail.com>
2021-09-13 16:12:37 -0300

    Adds print variables

--- main.py
+++ main.py
@@ -27,14 +27,18 @@ def plot_path(cost_map, start_node_id, goal_node_id, path):
 data_filename = "./australia.csv"
 start_node_id = 5
 goal_node_id = 219
+print_path = True
+do_plot_path = False
 
 cost_map = CostMap(data_filename)
 path_planner = PathPlanner(cost_map)
 path, cost = path_planner.a_star(start_node_id, goal_node_id)
 
 print("Cost: ", cost*1.1)
-# print("Path: ", path)
-# plot_path(cost_map, start_node_id, goal_node_id, path)
+if print_path:
+    print("Path: ", path)
+if do_plot_path:
+    plot_path(cost_map, start_node_id, goal_node_id, path)
 
 # Validation
 graph = nx.Graph()
